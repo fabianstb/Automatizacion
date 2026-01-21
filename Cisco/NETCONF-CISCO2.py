03b46815576311a14cb7a9805c56beb6e02fe415
fabianstb <fsantibanez.st@gmail.com>
2026-01-21 10:56:02 -0300

    Agregando ansible

--- Cisco/NETCONF-CISCO2.py
+++ Cisco/NETCONF-CISCO2.py
@@ -3,15 +3,24 @@ import netmiko
 import json
 from netmiko import ConnectHandler
 
-cisco1 = { "ip": "192.168.0.101",
+cisco1 = { "ip": "192.168.100.3",
             "device_type": "cisco_ios",
             "username": "cisco",
-            "password": "cisco123",
+            "password": "cisco123!",
 
 }
-comando = "show ip int bri"
+rev = "show ip int bri"
+comm1 = ["interface Loopback10","ip address 20.20.20.1 255.255.255.255 ","no shutdown"]
 
 with ConnectHandler(**cisco1) as net_connect:
-    salida = net_connect.send_command(comando)
+    salida = net_connect.send_command("show interface Loopback10")
+    #net_connect.send_config_set(comm1)
+    use_textfsm=True
 
 print(salida)
+
+with ConnectHandler(**cisco1) as net_connect:
+    salida = net_connect.send_config_set(comm1)
+
+
+
